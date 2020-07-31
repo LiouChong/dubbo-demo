6198d59abce79f95d9ee79ac5ed222c4ff21a6d9
liuchong <970009721@qq.com>
2020-07-31 15:00:18 +0800

    feat: base dubbo

---
+++ common-interface/src/test/java/com/dubbostudy/commoninterface/CommonInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dubbostudy.commoninterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonInterfaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
